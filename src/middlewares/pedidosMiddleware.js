0fa8646cb359e75befd70d450697eb35e15d483b
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-09 12:06:40 -0300

    feat: validação de dados pra cadastro de pedidos

---
+++ src/middlewares/pedidosMiddleware.js
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+const schema = z.object({
+    user_id: z.number().int(),
+    endereco_id: z.number().int(),
+    itens: z.array(z.object({
+        product_id: z.number().int(),
+        quantity: z.number().int().positive()
+    }))
+})
+
+function ValidarCadastro(req, res, next){
+    const cadastro = schema.safeParse(req.body)
+    //Verificar dados
+    if(!cadastro.success){
+        return res.status(400).json({
+            erros: cadastro.error.issues.map(issue => ({
+                campo: issue.path[0], // nome do campo que falhou
+                mensagem: issue.message // descrição do erro
+            }))
+        })
+    }
+    //dados válidos -> passa para o controller
+    next()
+}
+
+export default {ValidarCadastro}
--- src/route/pedidosRoutes.js
+++ src/route/pedidosRoutes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import pedidosController from '../controller/pedidosController.js'
+import pedidosMiddleware from '../middlewares/pedidosMiddleware.js'
 
 const router = Router()
 
-router.post('/', pedidosController.Criarpedido)
+router.post('/', pedidosMiddleware.ValidarCadastro, pedidosController.Criarpedido) //Rota pra validar e cadastrar pedidos
 
-router.get('/', pedidosController.listarPedidos)
+router.get('/', pedidosController.listarPedidos) //Rota pra listar todos pedidos cadastrados
 
-router.get('/:id', pedidosController.listarPedidosPorId)
+router.get('/:id', pedidosController.listarPedidosPorId) //Rota pra listar apenas o pedido requisitado pelo id
 
-router.put('/:id', pedidosController.EditarPedido)
+router.put('/:id', pedidosController.EditarPedido) //Rota pra editar apenas o pedido requisitado pelo id
 
-router.delete('/:id', pedidosController.DeletarProduto)
+router.delete('/:id', pedidosController.DeletarProduto) //Rota pra deletar apenas o pedido requisitado pelo id
 
 export default router
