f7df18695b86744d4e4511765fcacbe208f853d8
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-09 12:12:49 -0300

    feat: comentários //

--- src/route/enderecoRoutes.js
+++ src/route/enderecoRoutes.js
@@ -3,14 +3,14 @@ import enderecoController from '../controller/enderecoController.js'
 
 const router = Router()
 
-router.post('/', enderecoController.CadastrarEnderecos) //Rota post pra cadastros de endereços
+router.post('/', enderecoController.CadastrarEnderecos) //Rota pra validar e cadastros de endereços
 
-router.get('/', enderecoController.ListarEnderecos) //Rota get pra listagem de todos os endereços cadastrados
+router.get('/', enderecoController.ListarEnderecos) //Rota pra listagem de todos os endereços cadastrados
 
-router.get('/:id', enderecoController.ListarEndereco) //Rota get pra listagem de apenas um endereço requisitado
+router.get('/:id', enderecoController.ListarEndereco) //Rota pra listagem de apenas um endereço requisitado pelo id
 
-router.put('/:id', enderecoController.EditarEndereco) //Rota put pra edição de endereço cadastrado
+router.put('/:id', enderecoController.EditarEndereco) //Rota pra edição de apenas um endereço requisitado pelo id
 
-router.delete('/:id', enderecoController.DeletarEndereco) //Rota delete pra deletar endereços cadastrados
+router.delete('/:id', enderecoController.DeletarEndereco) //Rota pra deletar apenas um endereço requisitado pelo id
 
 export default router
