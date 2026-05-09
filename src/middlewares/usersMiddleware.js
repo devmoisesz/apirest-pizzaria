ff70f64ece3bf6df71f160db020ea5203321c8a0
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-09 11:20:33 -0300

    feat: comentários explicando código

--- src/middlewares/usersMiddleware.js
+++ src/middlewares/usersMiddleware.js
@@ -1,13 +1,19 @@
+// Middleware de validação que roda antes do controller
 import { z } from 'zod'
 
+//Schema define o formato esperado do body
 const schema = z.object({
     nome: z.string().min(3),
     email: z.string().email()
 })
 
 function ValidarCadastro(req, res, next){
+    //safeParse() valida os dados sem lançar exceção
     const resultado = schema.safeParse(req.body)
+    //verificar dados
     if(!resultado.success){
+        //dados inválidos -> retorna erros
+        //map pra simplificar o erro
         return res.status(400).json({
             erros: resultado.error.issues.map(issue => ({
                 campo: issue.path[0],
@@ -15,6 +21,7 @@ function ValidarCadastro(req, res, next){
             }))
         })
     }
+    //dados válidos -> passa para o controller
     next()
 }
 
