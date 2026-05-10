55217a3810c030ef5f78414027fd253aefce732e
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-10 14:06:54 -0300

    feat: definição da porta do servidor local

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ server.use(errorMiddleware.errosGlobais) // Servidor pra tratar erros globais
 
 server.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
-})
+}) 
