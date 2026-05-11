c805a9262b2b545e00fc4af1ad5cf02a2331b324
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-11 14:47:43 -0300

    feat: proteção de rotas categorias pra apenas Admin

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 function apenasAdmin(req, res, next){
-    if(req.usuario.papel !== 'aadmin'){
+    if(req.usuario.papel !== 'admin'){
         const erro = new Error('Acesso negado!')
         erro.status = 403
         return next(erro)
--- src/route/categoryRoutes.js
+++ src/route/categoryRoutes.js
@@ -2,19 +2,26 @@ import { Router } from "express";
 import categoryController from '../controller/categoryController.js'
 import categoryMiddleware from '../middlewares/categoryMiddleware.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
+import adminMiddleware from '../middlewares/adminMiddleware.js'
 
 const router = Router()
 
-router.post('/', authMiddleware.autenticarToken, categoryMiddleware.VerificarCadastro, categoryController.criarCategoria) //Rota pra validar e cadastro de categorias
+//Rota pra validar e cadastro de categorias(Apenas Admin tem acesso)
+router.post('/', authMiddleware.autenticarToken, adminMiddleware.apenasAdmin, categoryMiddleware.VerificarCadastro, categoryController.criarCategoria) 
 
-router.get('/', categoryController.listarCategorias) //Rota pra ler todas categorias cadastradas
+//Rota pra ler todas categorias cadastradas
+router.get('/', categoryController.listarCategorias) 
 
-router.get('/:id', categoryController.listarCategoriasPorId) //Rota pra ler apenas categoria requisitada pelo id
+//Rota pra ler apenas categoria requisitada pelo id
+router.get('/:id', categoryController.listarCategoriasPorId) 
 
-router.get('/:id/produtos', categoryController.ProdutoDaCategoria) //Rota pra ler os produtos da categoria
+//Rota pra ler os produtos da categoria
+router.get('/:id/produtos', categoryController.ProdutoDaCategoria) 
 
-router.put('/:id', authMiddleware.autenticarToken, categoryController.editar) // Rota pra editar categoria
+// Rota pra editar categoria (Apenas Admin tem acesso)
+router.put('/:id', authMiddleware.autenticarToken, adminMiddleware.apenasAdmin, categoryMiddleware.VerificarCadastro, categoryController.editar) 
 
-router.delete('/:id', authMiddleware.autenticarToken, categoryController.deletar) //Rota pra deletar categorias
+//Rota pra deletar categorias(Apenas Admin tem acesso)
+router.delete('/:id', authMiddleware.autenticarToken, adminMiddleware.apenasAdmin, categoryController.deletar) 
 
 export default router
