3d25a3e3b158507e2068836cf1729a264eb03d68
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-23 20:23:41 -0300

    refactor: rota pedidos, endereços retornando status de erro correto

--- src/service/enderecoService.js
+++ src/service/enderecoService.js
@@ -2,8 +2,12 @@ import enderecoRepository from '../repository/enderecoRepository.js'
 
 async function CadastrarEnderecos(user_id, cidade, rua, numero, bairro, complemento, cep) {
     //verificar se usuario requisitado existe
-    const id = await enderecoRepository.BuscarIDdoUsuario(user_id)
-    if(!id) throw new Error("Usuário não encontrado!")
+    const UsuarioId = await enderecoRepository.BuscarIDdoUsuario(user_id)
+    if(!UsuarioId) {
+        const error = new Error('Usuário não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await enderecoRepository.CadastrarEnderecos(user_id, cidade, rua, numero, bairro, complemento, cep)
 }
 
@@ -14,15 +18,27 @@ async function ListarEnderecos() {
 
 async function ListarEndereco(id) {
     const idEndereco = await enderecoRepository.BuscarIDdoEndereco(id)
-    if(!idEndereco) throw new Error("Endereço não Encontrado!")
+    if(!idEndereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await enderecoRepository.ListarEndereco(id)
 }
 
 async function ListarEnderecoGerenciado(idUser) {
     const userId = await enderecoRepository.BuscarIDdoUsuario(idUser)
-    if(!userId) throw new Error("Usuário não Encontrado!")
+    if(!userId) {
+        const error = new Error('Usuário não encontrado!')
+        error.status = 404
+        throw error
+    }
     const endereco = await enderecoRepository.ListarEnderecoGerenciado(idUser)
-    if(!endereco) throw new Error("Endereço não Encontrado!")
+    if(!endereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return endereco
 }
 
@@ -30,25 +46,41 @@ async function EditarEnderecoGerenciado(id_user, idEndereco, cidade, rua, numero
     const upEndereco = await enderecoRepository.EditarEnderecoGerenciado(
         id_user, idEndereco, cidade, rua, numero, bairro, complemento, cep
     )
-    if(!upEndereco) throw new Error("Não Encontrado!")
+    if(!upEndereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return upEndereco
 }
 
 async function EditarEndereco(id, cidade, rua, numero, bairro, complemento, cep) {
     const idEndereco = await enderecoRepository.BuscarIDdoEndereco(id)
-    if(!idEndereco) throw new Error("Endereço não Encontrado!")
+    if(!idEndereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await enderecoRepository.EditarEndereco(id, cidade, rua, numero, bairro, complemento, cep)
 }
 
 async function DeletarEnderecoGerenciado(idUsuario, idEndereco) {
     const enderecoId = await enderecoRepository.BuscarIDdoEndereco(idEndereco)
-    if(!enderecoId) throw new Error("Endereço não Encontrado!")
+    if(!enderecoId) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await enderecoRepository.DeletarEnderecoGenciado(idUsuario, idEndereco)
 }
 
 async function DeletarEndereco(id) {
     const idEndereco = await enderecoRepository.BuscarIDdoEndereco(id)
-    if(!idEndereco) throw new Error("Endereço não Encontrado!")
+    if(!idEndereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await enderecoRepository.DeletarEndereco(id)
 }
 
--- src/service/pedidosService.js
+++ src/service/pedidosService.js
@@ -2,11 +2,19 @@ import pedidosRepository from '../repository/pedidosRepository.js'
 
 async function Criarpedido(user_id, endereco_id, itens) {
     const endereco = await pedidosRepository.buscaEndereco(endereco_id)
-    if(!endereco) throw new Error("Endereço não encontrado!")
+    if(!endereco) {
+        const error = new Error('Endereço não encontrado!')
+        error.status = 404
+        throw error
+    }
 
     await Promise.all(itens.map(async (item) => {
             const produto = await pedidosRepository.buscaProduto(item.product_id)
-            if(!produto) throw new Error("Produto não Encontrado!")
+            if(!produto) {
+                const error = new Error('Produto não encontrado!')
+                error.status = 404
+                throw error
+            }
             item.unit_price = produto.price //Adiciona o preço no item
         }))
 
@@ -22,11 +30,19 @@ async function ListarHistorico(idCliente) {
 async function listarPedidoCliente(idCliente, idPedido) {
     // valida se o pedido existe
     const id_pedido = await pedidosRepository.buscarId(idPedido)
-    if(!id_pedido) throw new Error("Pedido não encontrado")
+    if(!id_pedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
 
     // valida se o pedido pertence ao cliente
     const pedido = await pedidosRepository.listarPedidoCliente(idCliente, idPedido)
-    if(!pedido) throw new Error("Pedido não encontrado")
+    if(!pedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
 
     return pedido
 }
@@ -38,13 +54,21 @@ async function listarPedidos() {
 
 async function listarPorId(id) {
     const idpedido = await pedidosRepository.buscarId(id)
-    if(!idpedido) throw new Error("Pedido não encontrado")
+    if(!idpedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await pedidosRepository.buscarPedido(id)
 }
 
 async function EditarPedido(id, status) {
     const idpedido = await pedidosRepository.buscarId(id)
-    if(!idpedido) throw new Error("Pedido não encontrado")
+    if(!idpedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
     return await pedidosRepository.EditarPedido(id, status)
 }
 
@@ -53,18 +77,39 @@ async function ClienteCancelarPedido(id_user, idPedido) {
     const pedido = await pedidosRepository.BuscarPedidoPorUsuario(id_user, idPedido)
 
     //Verificar se o pedido requisitado existe
-    if(!pedido) throw new Error("Pedido não encontrado!")
+    if(!pedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
     //Verificar se o pedido está pendente
-    if(pedido.status !== 'pendente') throw new Error("Não é possível cancelar um pedido que não esteja pendente.");
+    if(pedido.status !== 'pendente') {
+        const error = new Error('Não é possível cancelar um pedido que não esteja pendente.')
+        error.status = 409
+        throw error
+    }
 
     return await pedidosRepository.ClienteCancelarPedido(id_user, idPedido)
 }
 
 async function DeletarPedido(id) {
     const idpedido = await pedidosRepository.buscarId(id)
-    if(!idpedido) throw new Error("Pedido não encontrado!")
+    if(!idpedido) {
+        const error = new Error('Pedido não encontrado!')
+        error.status = 404
+        throw error
+    }
     const statusCancelamento = await pedidosRepository.statusPendente(id)
-    if(statusCancelamento.status !== "pendente") throw new Error("Não é possível cancelar um pedido que não esteja pendente.")
+    if(statusCancelamento.status === 'cancelado'){
+        const error = new Error('Pedido já cancelado!')
+        error.status = 409
+        throw error
+    }
+    if(statusCancelamento.status !== "pendente") {
+        const error = new Error('Não é possível cancelar um pedido que não esteja pendente.')
+        error.status = 409
+        throw error
+    }
     return await pedidosRepository.DeletarPedido(id)
 }
 
